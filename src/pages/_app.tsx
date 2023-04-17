e7186ae8a16365ca0b22a519b50d10e2c1613076
luis dourado <lfaddourado@gmail.com>
2023-04-16 23:25:57 -0300

    feat: add navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { ptBR } from "@clerk/localizations";
+import { dark } from "@clerk/themes";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Navbar } from "~/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      localization={ptBR}
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <Navbar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
